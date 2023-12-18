a42c1cceeb8a12462c329ba46272a8b77fad65f7
TestTesterThatTestsTestsTestTakersTake <jws09@mail.aub.edu>
2023-12-18 15:25:55 +0200

    stack navigation, tab navigation and functionality implemented

--- ReactNativeNavigationBasics/src/functions/SignUpValidation.ts
+++ ReactNativeNavigationBasics/src/functions/SignUpValidation.ts
@@ -1,31 +1,37 @@
 import {Alert} from 'react-native';
 import {getSignUpStore} from '../store/signUpStore';
 import {isValidEmail} from './EmailRegx';
-
+import {getUserArrayStore} from '../store/userArrayStore';
 export function signUpValidation(navigation: any) {
   const SignUpStore = getSignUpStore();
+  const UserArrayStore = getUserArrayStore();
   const email = SignUpStore.emailValue.get();
   const password = SignUpStore.passwordValue.get();
   const isSame = SignUpStore.passwordsAreSame.get();
+  const array = UserArrayStore.userArray;
 
   let errors = [];
 
   if (!isValidEmail(email)) {
     errors.push('Valid Email');
   }
+
+  if (array.find(user => user.email === email)) {
+    errors.push('Email already in use');
+  }
+
   if (!password) {
     errors.push('Password');
   }
 
-  if (errors.length > 0) {
-    Alert.alert('Data Missing', errors.join(', '), [{text: 'OK'}]);
-    return;
+  if (!isSame) {
+    errors.push("Passwords don't match");
   }
 
-  if (isSame) {
-    navigation.navigate('Details');
-  } else {
-    Alert.alert('Warning', "Password didn't match", [{text: 'OK'}]);
+  if (errors.length > 0) {
+    Alert.alert('Warning', errors.join(', '), [{text: 'OK'}]);
     return;
+  } else {
+    navigation.navigate('Details');
   }
 }
